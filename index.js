423c2a82a3bc8af939d9af21b4c0785a0805bb52
Joseph Rehfuss <josephrehfuss@L10241-FVFXVBL0HV22.local>
2020-02-12 13:13:17 -0800

    adding heroku config

--- index.js
+++ index.js
@@ -24,6 +24,73 @@ app.get("/api/animals",(req,res)=>{
         res.json(animals);
     })
 })
+app.get('/seedit',(req,res)=>{
+    const animals = [
+        {
+          "species": "zebra",
+          "class": "mammal",
+          "eatsMeat": false,
+          "weight": 200
+        },
+        {
+          "species": "lion",
+          "class": "mammal",
+          "eatsMeat": true,
+          "weight": 150
+        },
+        {
+          "species": "whale shark",
+          "class": "fish",
+          "eatsMeat": false,
+          "weight": 5000
+        },
+        {
+          "species": "penguin",
+          "class": "bird",
+          "eatsMeat": true,
+          "weight": 5
+        },
+        {
+          "species": "bald eagle",
+          "class": "bird",
+          "eatsMeat": true,
+          "weight": 10
+        },
+        {
+          "species": "banana slug",
+          "class": "insect",
+          "eatsMeat": false,
+          "weight": 1
+        },
+        {
+          "species": "manatee",
+          "class": "mammal",
+          "eatsMeat": false,
+          "weight": 75
+        },
+        {
+          "species": "gorilla",
+          "class": "mammal",
+          "eatsMeat": false,
+          "weight": 100
+        },
+        {
+          "species": "cat",
+          "class": "mammal",
+          "eatsMeat": true,
+          "weight": 4
+        },
+        {
+          "species": "frog",
+          "class": "amphibian",
+          "eatsMeat": false,
+          "weight": 2
+        }
+      ]
+    db.Animal.bulkCreate(animals).then(data=>{
+        console.log("dataSeeded");
+    })
+})
 app.get("/api/animals/class/:classname",(req,res)=>{
     db.Animal.findAll({
         where:{
